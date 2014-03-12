64bf34fa70003deb25dcc599f131459d7c0d3787
Bob Rossi <bob@brasko.net>
2014-03-11 21:47:00 -0400

    Remove unused fixture code (readd if necessary).

--- src/progs/test_suite/fixture.cpp
+++ src/progs/test_suite/fixture.cpp
@@ -2,15 +2,6 @@
 #include "catch.hpp"
 #include "fixture.h"
 
-Fixture::Fixture()
-{
-    copyTestData();
-}
-
-Fixture::~Fixture()
-{
-}
-
 std::string
 Fixture::testName()
 {
@@ -41,79 +32,4 @@ Fixture::sourceTestDir()
     std::string datadir = data();
     datadir = datadir + "/" + testName();
     return datadir;
-}
-
-std::string
-Fixture::dest()
-{
-    std::string builddir = GDBWIRE_ABS_TOP_BUILDDIR;
-    builddir += "/results";
-    return builddir;
-}
-
-std::string
-Fixture::destTestDir()
-{
-    std::string destdir = dest();
-    destdir = destdir + "/" + testName();
-    return destdir;
-}
-
-void
-Fixture::copy(const std::string &src, const std::string &dest, bool recursive)
-{
-#if 0
-    if (!exists(src)) {
-        // throw file not found exception src
-    } else if (!exists(dest.parent_path())) {
-        // throw file not found exception for dest parent path
-    } else if (is_directory(src) && !recursive) {
-        // throw exception if trying to copy a directory and not recursive
-    }
-
-    // Determine the target path name
-    path target = dest;
-    if (is_directory(dest)) {
-        target /= src.filename();
-    }
-
-    // Do the copy (separate into recursive and non-recursive copy)
-    if (recursive && is_directory(src)) {
-        // Does this throw an exception on failure?
-        copy_directory(src, target);
-        for (directory_iterator end, iter(src);  iter != end; ++iter) {
-            copy(iter->path(), target, recursive);
-        }
-    } else {
-        copy(src, target);
-    }
-#endif
-}
-
-void
-Fixture::copyTestData()
-{
-#if 0
-    path sourceTestDirectory = sourceTestDir();
-    path destTestDirectory = destTestDir();
-    path destTestCaseDirectory(destTestDirectory.parent_path());
-
-    // Delete the test directory if it exists, to start fresh
-    if (exists(destTestDirectory)) {
-        remove_all(destTestDirectory.filename());
-    }
-    
-    // The Test Case directory may not exist if this is
-    // the first test to run in this Test Case, create it then.
-    if (!exists(destTestCaseDirectory)) {
-        //VASSERT(QDir().mkpath(destTestCaseDirectory.path()));
-    }
-
-    // Finally, if the Test directory exists in the source tree then
-    // copy it to the destination tree. Otherwise do nothing as the test
-    // must not require files.
-    if (exists(sourceTestDirectory)) {
-        copy(sourceTestDirectory, destTestCaseDirectory);
-    }
-#endif
 }
--- src/progs/test_suite/fixture.h
+++ src/progs/test_suite/fixture.h
@@ -9,42 +9,34 @@
  * This test fixture provides a simple framework for dealing with
  * the file system while performing a unit test.
  *
- * It provides a standard location to read test data and a
- * standard location to write test data. It also provides the
- * ability to automatically copy test data from the source tree
- * to the test working directory.
+ * It provides a standard location to read test data from the src tree.
  *
  * Any test inheriting from this test fixture will automatically
- * perform the copy of the test data from the source directory
- * to the destination directory in the constructor of this class.
+ * know where to get any necessary related data files required for testing.
  *
  * The source directory for the test case comes from,
  *   $abs_top_srcdir/progs/test_suite/data/TestName
- * The destination directory for the test case will be,
- *   $abs_top_builddir/test_suite/TestName
- *
  * Please note that TestName is a hierarchical name. See testName()
  * for more information on this.
  */
 class Fixture {
     public:
 
-        Fixture();
-        virtual ~Fixture();
+        virtual ~Fixture() {}
 
         /**
          * Get the name of the test currently running.
          *
          * The name of the test is hierarchical. The format is
-         * root/node1/node2/.../leaf. The slash separator is convient as
-         * it allows you to split up the test case into a hierarchy and
-         * it also can be used as a unique directory name.
-         *
-         * An example would be,
+         * root/node1/node2/.../leaf. An example would be,
          *   GdbmiTest/basic
          * or
-         *   append_cstr/null_value 
-         *   append_cstr/null_instance 
+         *   GdbwireStringTest/append_cstr/null_value 
+         *   GdbwireStringTest/append_cstr/null_instance 
+         *
+         * When using the macro TEST_F below please note that the fixture
+         * name is always the first piece of the test name. So in the
+         * examples above that means GdbmiTest or GdbwireStringTest.
          *
          * @return
          * The test name of the test currently running.
@@ -77,7 +69,7 @@ class Fixture {
          * Get the test specific directory in the source directory.
          *
          * For example
-         *   $abs_top_srcdir/progs/test_suite/data/TestCaseName/TestName.
+         *   $abs_top_srcdir/progs/test_suite/data/TestName.
          *
          * @return
          * The absolute path to the test specific directory contained in the
@@ -85,67 +77,6 @@ class Fixture {
          * This may be read-only.
          */
         std::string sourceTestDir();
-        
-        /**
-         * Get the destination directory this test suite is running in.
-         *
-         * This is useful to allow tests to create temporary files and
-         * folders as needed to complete their tests.
-         *
-         * For example $abs_top_builddir/results.
-         *
-         * @return
-         * The absolute path to the destination directory this test suite is
-         * running against. This is intended to be writable.
-         */
-        std::string dest();
-
-        /** 
-         * Get the test specific directory in the destination directory.
-         *
-         * For example $abs_top_builddir/results/TestCaseName/TestName.
-         *
-         * @return
-         * The absolute path to the test specific directory contained in the
-         * destination directory this test suite is running against.
-         * This is intended to be writable.
-         */
-        std::string destTestDir();
-
-
-        /**
-         * Copy the source directory to the destination directory.
-         *
-         * This is a non recursive directory copy. It only copies a
-         * single source directory and all the files in it to the
-         * destionation directory.
-         *
-         * The destination directory may exist. If it doesn't, it
-         * will be created. The destination directory may not contain
-         * a folder in it that is the basename of the source directory.
-         * This function is required to make and create that directory.
-         *
-         * @param src
-         * The src directory to copy, and all the files in it.
-         *
-         * @param dest
-         * The destination directory.
-         */
-        void copy(const std::string &src, const std::string &dest,
-                bool recursive = false);
-
-    private:
-        /**
-         * Copy the test data from the src dir to the destination dir.
-         *
-         * Each test case may have some tests that contain data on disk
-         * necessary to run the test.
-         *
-         * This function copies the data from the src tree into the
-         * destination tree and sets the destination test directory
-         * member variable.
-         */
-        void copyTestData();
 };
 
 // A convience macro for creating unit tests.
