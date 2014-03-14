fff361c4992f27785144b9a3893aa6637ba365e4
Bob Rossi <bob@brasko.net>
2014-03-13 22:13:18 -0400

    Add the GdbmiPtTest/oob_record/stream/console/characters unit test.

--- src/progs/test_suite/gdbmi/gdbmi_pt.cpp
+++ src/progs/test_suite/gdbmi/gdbmi_pt.cpp
@@ -105,14 +105,66 @@ TEST_CASE_METHOD_N(GdbmiPtTest, basic)
  */
 TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/console/basic)
 {
+    std::string expected = "\"Hello World console output\"";
     REQUIRE(output->oob_record);
         struct gdbmi_oob_record *oob = output->oob_record;
         REQUIRE(oob->kind == GDBMI_STREAM);
             struct gdbmi_stream_record *stream = oob->variant.stream_record;
             REQUIRE(stream);
             REQUIRE(stream->kind == GDBMI_CONSOLE);
-            std::string expected =
-                "\"Hello World console output\"";
+            REQUIRE(expected == stream->cstring);
+        REQUIRE(!oob->next);
+    REQUIRE(!output->result_record);
+    REQUIRE(!output->next);
+}
+
+/**
+ * All possible characters in the console output stream.
+ *
+ * The basic idea behind this test is to print a character array with
+ * every possible value. For example,
+ *   char chars[256];
+ *   for (i = 0; i < 256; ++i) {
+ *     chars[i] = (char)i;
+ *   }
+ * The MI output converts the char value 0 to \\000 and 1 to \\001, etc.
+ * So it essentially escapes the backslashes. In C, I have to escape them
+ * again to compare the values.
+ * 
+ */
+TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/console/characters)
+{
+    std::string expected =
+        "\"$1 = "
+        "\\\"\\\\000\\\\001\\\\002\\\\003\\\\004\\\\005\\\\006\\\\a"
+        "\\\\b\\\\t\\\\n\\\\v\\\\f\\\\r\\\\016\\\\017"
+        "\\\\020\\\\021\\\\022\\\\023\\\\024\\\\025\\\\026\\\\027"
+        "\\\\030\\\\031\\\\032\\\\033\\\\034\\\\035\\\\036\\\\037"
+        " !\\\\\\\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "[\\\\\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\\\177"
+        "\\\\200\\\\201\\\\202\\\\203\\\\204\\\\205\\\\206\\\\207"
+        "\\\\210\\\\211\\\\212\\\\213\\\\214\\\\215\\\\216\\\\217"
+        "\\\\220\\\\221\\\\222\\\\223\\\\224\\\\225\\\\226\\\\227"
+        "\\\\230\\\\231\\\\232\\\\233\\\\234\\\\235\\\\236\\\\237"
+        "\\\\240\\\\241\\\\242\\\\243\\\\244\\\\245\\\\246\\\\247"
+        "\\\\250\\\\251\\\\252\\\\253\\\\254\\\\255\\\\256\\\\257"
+        "\\\\260\\\\261\\\\262\\\\263\\\\264\\\\265\\\\266\\\\267"
+        "\\\\270\\\\271\\\\272\\\\273\\\\274\\\\275\\\\276\\\\277"
+        "\\\\300\\\\301\\\\302\\\\303\\\\304\\\\305\\\\306\\\\307"
+        "\\\\310\\\\311\\\\312\\\\313\\\\314\\\\315\\\\316\\\\317"
+        "\\\\320\\\\321\\\\322\\\\323\\\\324\\\\325\\\\326\\\\327"
+        "\\\\330\\\\331\\\\332\\\\333\\\\334\\\\335\\\\336\\\\337"
+        "\\\\340\\\\341\\\\342\\\\343\\\\344\\\\345\\\\346\\\\347"
+        "\\\\350\\\\351\\\\352\\\\353\\\\354\\\\355\\\\356\\\\357"
+        "\\\\360\\\\361\\\\362\\\\363\\\\364\\\\365\\\\366\\\\367"
+        "\\\\370\\\\371\\\\372\\\\373\\\\374\\\\375\\\\376\\\\377\\\"\"";
+
+    REQUIRE(output->oob_record);
+        struct gdbmi_oob_record *oob = output->oob_record;
+        REQUIRE(oob->kind == GDBMI_STREAM);
+            struct gdbmi_stream_record *stream = oob->variant.stream_record;
+            REQUIRE(stream);
+            REQUIRE(stream->kind == GDBMI_CONSOLE);
             REQUIRE(expected == stream->cstring);
         REQUIRE(!oob->next);
     REQUIRE(!output->result_record);
@@ -124,14 +176,13 @@ TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/console/basic)
  */
 TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/target/basic)
 {
+    std::string expected = "\"Hello World target output\"";
     REQUIRE(output->oob_record);
         struct gdbmi_oob_record *oob = output->oob_record;
         REQUIRE(oob->kind == GDBMI_STREAM);
             struct gdbmi_stream_record *stream = oob->variant.stream_record;
             REQUIRE(stream);
             REQUIRE(stream->kind == GDBMI_TARGET);
-            std::string expected =
-                "\"Hello World target output\"";
             REQUIRE(expected == stream->cstring);
         REQUIRE(!oob->next);
     REQUIRE(!output->result_record);
@@ -143,14 +194,13 @@ TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/target/basic)
  */
 TEST_CASE_METHOD_N(GdbmiPtTest, oob_record/stream/log/basic)
 {
+    std::string expected = "\"Hello World log output\"";
     REQUIRE(output->oob_record);
         struct gdbmi_oob_record *oob = output->oob_record;
         REQUIRE(oob->kind == GDBMI_STREAM);
             struct gdbmi_stream_record *stream = oob->variant.stream_record;
             REQUIRE(stream);
             REQUIRE(stream->kind == GDBMI_LOG);
-            std::string expected =
-                "\"Hello World log output\"";
             REQUIRE(expected == stream->cstring);
         REQUIRE(!oob->next);
     REQUIRE(!output->result_record);
